a81aebe1fd0b7a490a3083ab0b1ca48a9277872b
Younes Barka <younes1929@outlook.com>
2026-03-14 14:00:29 +0100

    Delete app/build directory

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_younes_financetracker_FinanceTrackerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.younes.financetracker.FinanceTrackerApp"
-)
-public final class _com_younes_financetracker_FinanceTrackerApp {
-}
